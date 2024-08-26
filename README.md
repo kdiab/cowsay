b21aae73912591e95e1496dca0f7235fc9050024
kdiab <khalid@diab.cc>
2024-08-26 13:41:06 -0400

    cowsay without the cow

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cowsay re-implementation in go
+
+I re-implemented lolcat and fortune so it's only fair that the trilogy is completed.
+
+---
+
+# Usage
+
+This command is intended to be used with pipes.
+
+Usage: echo "Hello, world!" | cowsay
---
+++ cowsay.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"unicode/utf8"
+)
+
+func buildCloud(lines []string, width int) string {
+	var ret []string
+	borders := []string{"/", "\\", "|", "<", ">"}
+	count := len(lines)
+
+	top := " " + strings.Repeat("_", width+2)
+	bottom := " " + strings.Repeat("-", width+2)
+
+	ret = append(ret, top)
+	if count == 1 {
+		s := fmt.Sprintf("%s %s %s", borders[3], lines[0], borders[4])
+		ret = append(ret, s)
+	} else {
+		s := fmt.Sprintf("%s %s %s", borders[0], lines[0], borders[1])
+		ret = append(ret, s)
+		i := 1
+		for ; i < count-1; i++ {
+			s := fmt.Sprintf("%s %s %s", borders[2], lines[i], borders[2])
+			ret = append(ret, s)
+		}
+		s = fmt.Sprintf("%s %s %s", borders[1], lines[i], borders[0])
+		ret = append(ret, s)
+	}
+
+	ret = append(ret, bottom)
+	return strings.Join(ret, "\n")
+}
+
+func tabsToSpaces(lines []string) []string {
+	var ret []string
+	for _, c := range lines {
+		if c == "\t" {
+			c = strings.Replace(c, "\t", "    ", -1)
+			ret = append(ret, c)
+		} else {
+			ret = append(ret, c)
+		}
+	}
+	return ret
+}
+
+func calculateMaxWidth(lines []string) int {
+	w := 0
+	for _, l := range lines {
+		len := utf8.RuneCountInString(l)
+		if len > w {
+			w = len
+		}
+	}
+	return w
+}
+
+func normalizeString(lines []string, width int) []string {
+	var ret []string
+	for _, l := range lines {
+		s := l + strings.Repeat(" ", width-utf8.RuneCountInString(l))
+		ret = append(ret, s)
+	}
+	return ret
+}
+
+func main() {
+	info, _ := os.Stdin.Stat()
+	var output []string
+
+	if info.Mode()&os.ModeCharDevice != 0 {
+		fmt.Println("The command is intended to work with pipes.")
+		fmt.Println("Usage: echo \"Hello, world!\" | cowsay")
+		os.Exit(1)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, _, err := reader.ReadLine()
+		if err != nil && err == io.EOF {
+			break
+		}
+		output = append(output, string(input))
+	}
+
+	lines := tabsToSpaces(output)
+	maxWidth := calculateMaxWidth(lines)
+	normalizedString := normalizeString(lines, maxWidth)
+	cloud := buildCloud(normalizedString, maxWidth)
+	fmt.Println(cloud)
+}
